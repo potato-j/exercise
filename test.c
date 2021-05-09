bb54100bcef238f7197b7049777af79f6f8973d1
potato-j <2087263877@qq.com>
2021-05-09 18:17:13 +0800

    首次使用GitHub,一切顺利

---
+++ test.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int i = 0;
+	for (i = 0; i < 100; i++){
+		printf("%d\n", i);
+	}
+	system("pause");
+	return 0;
+}
